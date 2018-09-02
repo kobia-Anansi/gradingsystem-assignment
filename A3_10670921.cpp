b25aea4782dafdb01469f2fa45c78c3444bb6591
kobia-Anansi <35210031+kobia-Anansi@users.noreply.github.com>
2018-09-02 05:12:43 -1200

    Update and rename stanford.cpp to A3_10670921.cpp

--- A3_10670921.cpp
+++ A3_10670921.cpp
@@ -9,7 +9,7 @@ int main(){
  	cout<<"Please enter a numeric value"<<endl;
  }
     string grade[6] = { "B+", "B", "C+", "C", "D", "E" }; int max=79; int min=75; x = abs(x);
-    for (int i = 0; i < 6; i++){ //the absolute value of x is taken to prevent errors such as negative numbers and % signs
+    for (int i = 0; i < 6; i++){ //the absolute value of x is taken to prevent errors such as negative numbers and % signs.
         if (x >= min and x <= max){ //each scope within the for loop returns 0 so that when the grade for the value inputed
             cout << "You had a " << grade[i] << endl;// is found, the for loop discontinues to reduce program execution time.
             return 0;
